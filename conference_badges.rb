31c2ec5af1bda4c44b3bae94c29e69043edbadb2
Zaire  Swailes <swaileszaire@gmail.com>
2019-12-16 11:54:51 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,4 +3,7 @@ puts "Hello my name is #{name}."
 end
 
 def batch_badge_creator
- ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"] 
+ name = ["Edger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"] 
+name.each do |phrase|
+  puts "Hello my name is #{name}
+end
